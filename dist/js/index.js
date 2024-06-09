6fdf98045d52cb8d3b40116aa9fe497ae4f35f7c
Yura <stauber.j@mail.ru>
2024-06-09 14:26:40 +0300

    Pages have been completely redesigned except select and script with photos

--- dist/js/index.js
+++ dist/js/index.js
@@ -17,4 +17,35 @@ document.addEventListener("DOMContentLoaded", () => {
       mobileMenu.classList.remove("mobile-menu_active");
     });
   });
+
+  //modals
+  const modal = document.querySelector(".modal");
+  const consultModal = document.querySelector(".modal__free-consult");
+  const calcModal = document.querySelector(".modal__calc");
+
+  const consultBtns = document.querySelectorAll(".btn-consult");
+  const calcBtns = document.querySelectorAll(".btn-calc");
+
+  const modalCloser = document.querySelectorAll(".modal__closer");
+
+  function openModal(arr, modalPopUp, modalClass) {
+    arr.forEach((item) => {
+      item.addEventListener("click", () => {
+        modal.classList.add("modal_active");
+        modalPopUp.classList.add(modalClass);
+        document.querySelector("body").style.overflow = "hidden";
+
+        modalCloser.forEach((closer) => {
+          closer.addEventListener("click", () => {
+            modal.classList.remove("modal_active");
+            modalPopUp.classList.remove(modalClass);
+            document.querySelector("body").style.overflow = "scroll";
+          });
+        });
+      });
+    });
+  }
+
+  openModal(consultBtns, consultModal, "free-consult_active");
+  openModal(calcBtns, calcModal, "calc_active");
 });
